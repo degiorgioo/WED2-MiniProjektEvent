9bfb4134e0b3c09c35ceccf1641274281f1d091b
bugproduction <dave_degiorgio@hotmail.com>
2015-10-29 11:52:11 +0100

    Display all events - Basic controller implementation

--- webapp/source/js/main.js
+++ webapp/source/js/main.js
@@ -1,3 +1,26 @@
-window.onload = function () {
-	//Some magical code here...
-};
+
+    var EventRepository = {
+      getAllEvents: function($http, successCallback) {
+          $http.get('http://localhost:8080/api/events')
+              .success(function(data)
+              {
+                  successCallback( data['events'] );
+
+              }).error(function()
+              {
+                  console.log("not successfull");
+              });
+      }
+    };
+
+    var EventListController = function($scope, $http){
+        this.$inject = ['$scope'];
+
+        EventRepository.getAllEvents($http, function(data){
+           $scope.events = data;
+        });
+
+    };
+
+    var EventApp = angular.module('EventApp', []);
+    EventApp.controller('EventListController', EventListController);
